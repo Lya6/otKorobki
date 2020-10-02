92cd0f425c6e246432e65015f9397ca4891c6d26
ToXaGIT <liasnikov99@gmail.com>
2020-10-02 10:34:17 +0300

    new_store

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,19 +3,24 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class Constants {
-  static final String baseUrl = "https://meridian-llc.ru/";
+  static final String baseUrl = "https://korzinka.pro/";
   //https://korzinka.pro/
   //https://betaotkorobki.otkorobki.ru/;
   //https://meridian-llc.ru/
   //https://maidan.otkorobki.ru/
 
-  //Meridian
-  static final color = Colors.red;
-  static final colorSecond = Colors.orange;
-  static final image = AssetImage('images/meridian.png');
+  // //Meridian
+  // static final color = Colors.red;
+  // static final colorSecond = Colors.orange;
+  // static final image = AssetImage('images/meridian.png');
 
   //Korobka
   // static final color = Color(0xFF00b3b3);
   // static final colorSecond = Colors.blue;
   // static final image = AssetImage('images/logo_beta.png');
+
+  //Korzinka
+  static final color = Color(0xFF5a6078);
+  static final colorSecond = Colors.grey;
+  static final image = AssetImage('images/logo_korzinka.png');
 }
