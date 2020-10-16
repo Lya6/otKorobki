c699025444a19862b227bcc2a5316c02898fa1eb
ToXaGIT <liasnikov99@gmail.com>
2020-10-16 13:22:09 +0300

    Fix bugs

--- android/app/src/main/kotlin/com/korzinka/template_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.korzinka.template_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/meridians/templateapplication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.meridians.templateapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
